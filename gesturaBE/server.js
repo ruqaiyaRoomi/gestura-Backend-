938fb355152b2a0bd77affc5636b2a92960ad15f
RuqaiyaRoomi <ruqaiyaroomi.com>
2026-04-13 23:16:12 +0400

    Fix Express CORS wildcard route issue

--- gesturaBE/server.js
+++ gesturaBE/server.js
@@ -17,7 +17,6 @@ app.set("port", 3000);
 const cors = require('cors')
 
 app.use(cors())
-app.options("*", cors())
 
 const uri = "mongodb+srv://ruqaiyah:RR1026@ug.9ogfhhl.mongodb.net/?appName=UG";
 const client = new MongoClient(uri);
