9f02f9698a6db5324a079fb1892fa93a41bb6aa5
RuqaiyaRoomi <ruqaiyaroomi.com>
2026-04-13 18:52:29 +0400

    edited file

--- gesturaBE/server.js
+++ gesturaBE/server.js
@@ -20,6 +20,9 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  
+
   next();
 });
 
